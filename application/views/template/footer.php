0df19e682f94b4ee612053cacc067dce9f2bc1ef
Tim Nguyen <civilordergone@gmail.com>
2013-05-09 20:23:32 -0400

    fixed footer and added github link

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,10 +1,11 @@
    <div id="footer">
       <div id="footer-wrapper">
         <div class="info links">
-          Copyright 2013 <a href="http://timnuwin.com/cmsFire">CMS Fire</a><br>                    
+          Copyright 2013 <a href="http://cmsFire.timnuwin.com">CMS Fire</a><br>                    
         </div>
         <div class="statement">Built using CMS Fire</div>
         <div class="links">          
+		  <a class="linkedIn" href="https://github.com/civilordergone/cmsfire">github</a>
 		  <a class="linkedIn" href="http://www.linkedin.com/in/iamtimnguyen">linkedIn</a>
           <a class="facebook" href="https://www.facebook.com/TimNuwinsInMemoryOutput">Facebook</a>
         </div>
